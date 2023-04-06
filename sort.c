accfc3df85c5d3c08f46ee48136127e88297bbb2
Miguel <belikemiggie@gmail.com>
2023-04-05 20:06:38 -0400

    Added heapsort and mergesort functions, and edited the print function to display correct data.

--- sort.c
+++ sort.c
@@ -1,20 +1,158 @@
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int extraMemoryAllocated;
 
 // implements heap sort
 // extraMemoryAllocated counts bytes of memory allocated
-void heapSort(int arr[], int n)
+
+void swap(int *rootP, int* largestP)
 {
+	int temp = *rootP;
+	*rootP = *largestP;
+	*largestP = temp;
 }
 
+void heapify(int arr[], int N, int i)
+{
+	//Find the largest among root, left child, and right child
+	//Initialize largest as root
+	int largest = i;
 
+	int left = 2 * i + 1; //index of the left child in the array
+	int right = 2 * i + 2; //index of the right child in the array
+
+	//if the left child is larger than the root
+	if(left < N && arr[left] > arr[largest])
+	{
+		largest = left;
+	}
+
+	//if the right child is larger than the largest so far
+	if(right < N && arr[right] > arr[largest])
+	{
+		largest = right;
+	}
+
+	//Swap and continue heapifying if root is not the largest
+
+	//if the largest is not root
+	if(largest != i)
+	{
+		swap(&arr[i], &arr[largest]);
+		
+		//recursively heapify the affected sub-tree
+		heapify(arr, N, largest);
+	}
+}
+void heapSort(int arr[], int n)
+{
+	//Building the binary max heap
+	for(int i = n/2 - 1; i >= 0; i--)
+	{
+		heapify(arr, n, i);
+	}
+
+	//sort the max heap
+	for(int i = n - 1; i >= 0; i--)
+	{
+		//swap the root node and the last leaf node
+		int temp = arr[i];
+		swap(&arr[i], &arr[0]);
+		
+		//again heapify the max heap from the root
+		heapify(arr, i, 0);
+	
+	}
+
+
+}
+
+void merge(int arr[], int l, int m, int r)
+{
+	int i;
+	int j;
+	int k;
+	int leftSize = m - l + 1;
+	int rightSize = r - m;
+
+	//Temporary Arrays
+	int *leftSideArray = (int*)malloc(leftSize * sizeof(int));
+	extraMemoryAllocated += leftSize * sizeof(int);
+
+	int *rightSideArray = (int*)malloc(rightSize * sizeof(int));
+	extraMemoryAllocated += rightSize * sizeof(int);
+
+	//Copying data to the temporary arrays
+
+	for(i = 0; i < leftSize; i++)
+	{
+		leftSideArray[i] = arr[l + i];
+	}
+	
+	for(j = 0; j < rightSize; j++)
+	{
+		rightSideArray[j] = arr[m + 1 + j];
+	}
+
+	//Merge the temp arrays back into arr[]
+
+	i = 0;
+	j = 0;
+	k = l;
+
+	while(i < leftSize && j < rightSize)
+	{
+		if(leftSideArray[i] <= rightSideArray[j])
+		{
+			arr[k] = leftSideArray[i];
+			i++;
+		}
+		else
+		{
+			arr[k] = rightSideArray[j];
+			j++;
+		}
+		
+		k++;
+
+	}
+
+	//Copy the remaining elements of the leftSideArray
+	while(i < leftSize)
+	{
+		arr[k] = leftSideArray[i];
+		i++;
+		k++;
+	}
+
+	//Copy the remaining elements of the rightSideArray
+	while(j < rightSize)
+	{
+		arr[k] = rightSideArray[j];
+		j++;
+		k++;
+	}
+
+	free(leftSideArray);
+	free(rightSideArray);
+
+}
 // implement merge sort
 // extraMemoryAllocated counts bytes of extra memory allocated
 void mergeSort(int pData[], int l, int r)
 {
+	if(l < r)
+	{
+		int m = (l + r)/2; //Get the midpoint
+		//Sort the first and second halves
+		mergeSort(pData, l, m);
+		mergeSort(pData, m+1, r);
+		merge(pData, l, m, r);
+
+	}
 }
 
 // parses input file to an integer array
@@ -22,27 +160,19 @@ int parseData(char *inputFileName, int **ppData)
 {
 	FILE* inFile = fopen(inputFileName,"r");
 	int dataSz = 0;
-	int i, n, *data;
 	*ppData = NULL;
+	
 	
 	if (inFile)
 	{
 		fscanf(inFile,"%d\n",&dataSz);
 		*ppData = (int *)malloc(sizeof(int) * dataSz);
 		// Implement parse data block
-		if (*ppData == NULL)
+		for(int i = 0; i < dataSz; i++)
 		{
-			printf("Cannot allocate memory\n");
-			exit(-1);
+			fscanf(inFile, "%d", *ppData + i);
 		}
-		for (i=0;i<dataSz;++i)
-		{
-			fscanf(inFile, "%d ",&n);
-			data = *ppData + i;
-			*data = n;
-		}
-
-		fclose(inFile);
+		
 	}
 	
 	return dataSz;
@@ -52,18 +182,33 @@ int parseData(char *inputFileName, int **ppData)
 void printArray(int pData[], int dataSz)
 {
 	int i, sz = dataSz - 100;
-	printf("\tData:\n\t");
-	for (i=0;i<100;++i)
-	{
-		printf("%d ",pData[i]);
-	}
-	printf("\n\t");
 	
-	for (i=sz;i<dataSz;++i)
+
+	printf("\tData:\n\t");
+	if(dataSz > 100)
 	{
-		printf("%d ",pData[i]);
+		for (i=0;i<100;++i)
+		{
+			printf("%d ",pData[i]);
+		}
+		printf("\n\t");
+	
+	
+	
+		for (i=sz;i<dataSz;++i)
+		{
+			printf("%d ",pData[i]);
+		}
+		printf("\n\n");
 	}
-	printf("\n\n");
+	else
+	{
+		for (i=0;i < dataSz ;++i)
+		{
+			printf("%d ",pData[i]);
+		}
+		printf("\n");
+	}
 }
 
 int main(void)
